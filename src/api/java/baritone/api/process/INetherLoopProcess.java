8eb36980ae72f88fe0beb0ba562ace75a2c9ed1c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-04-29 11:14:33 -0500

    Add NetherLoop API

---
+++ src/api/java/baritone/api/process/INetherLoopProcess.java
@@ -0,0 +1,10 @@
+package baritone.api.process;
+
+import net.minecraft.util.math.BlockPos;
+public interface INetherLoopProcess extends IBaritoneProcess
+{
+	void netherLoop();
+	void setNetherEntryPoint(BlockPos playerPos);
+	void setNetherExitPoint(BlockPos playerPos);
+
+}
