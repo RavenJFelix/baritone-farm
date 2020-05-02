b49f47f7395e078dc2d917238915cc339a61412a
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-05-02 08:54:31 -0500

    Add BlockZone class

--- src/main/java/baritone/process/NetherLoopProcess.java
+++ src/main/java/baritone/process/NetherLoopProcess.java
@@ -46,6 +46,38 @@ import java.util.Optional;
 import java.util.Collection;
 public final class NetherLoopProcess extends BaritoneProcessHelper implements INetherLoopProcess
 {
+	public class BlockZone
+	{
+		private BlockPos corner1;
+		private BlockPos corner2;
+
+		BlockZone(BlockPos newCorner1, BlockPos newCorner2)
+		{
+			corner1 = newCorner1;
+			corner2 = newCorner2;
+		}
+
+		public BlockPos getCorner1()
+		{
+			return corner1;
+		}
+
+		public BlockPos getCorner2()
+		{
+			return corner2;
+		}
+
+		public void setCorner1(BlockPos newCorner1)
+		{
+			corner1 = newCorner1;
+		}
+
+		public void setCorner2(BlockPos newCorner2)
+		{
+			corner2 = newCorner2;
+		}
+
+	}
 
 
  	private List <BlockPos>portalFrameObsidian = new ArrayList<>();
@@ -333,6 +365,7 @@ public final class NetherLoopProcess extends BaritoneProcessHelper implements IN
 
 		}
 	}
+
 
 	private void updateObsidianLocation()
 	{
